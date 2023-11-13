e3a002369d260c5414ca19a4a2ef59bb1f7e6cdf
Fernando Goncalves <fgoncalves@schubergphilis.com>
2023-11-13 14:46:35 +0100

    fix: adds missing config alias for AWS provider

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       version               = ">= 3.67"
-      configuration_aliases = [aws.kms]
+      configuration_aliases = [aws.kms, aws.dns_query_logging]
     }
   }
 
